dd4482b2fd4ca9ec868e274a2ca4ec727ce500c9
DoctorPlague <jjlyonsschool@gmail.com>
2016-10-27 15:18:39 +1300

    +major update

--- PMagicGuild/src/com/plaguedoctor/oldschoolscripts/pmagicguild/Main.java
+++ PMagicGuild/src/com/plaguedoctor/oldschoolscripts/pmagicguild/Main.java
@@ -3,6 +3,8 @@ package com.plaguedoctor.oldschoolscripts.pmagicguild;
 import org.osbot.rs07.api.map.Area;
 import org.osbot.rs07.api.map.Position;
 import org.osbot.rs07.api.model.NPC;
+import org.osbot.rs07.api.ui.RS2Widget;
+import org.osbot.rs07.api.ui.Tab;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 import org.osbot.rs07.utility.ConditionalSleep;
@@ -10,7 +12,8 @@ import org.osbot.rs07.utility.ConditionalSleep;
 
 
 import java.awt.*;
-import java.util.LinkedList; 
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom; 
 
  
 @ScriptManifest(name = "PMagicGuild", author = "Plague Doctor", version = 1.0, info = "Buys runes and battlestaves", logo = "")
@@ -19,35 +22,37 @@ public class Main extends Script {
 	// Variables
 	
 	Area magicGuild = new Area(new Position(2584, 3094, 1), new Position(2596, 3081, 1));
-	Area yanilleBank = new Area(2616, 3088, 2609, 3097);
-	NPC Store = npcs.closest("Magic Store owner");
+	Area yanilleBank = new Area(2616, 3088, 2609, 3097);	
+	String items[] = new String[] {"Death rune", "Nature rune", "Battlestaff",  };
+	int rand = ThreadLocalRandom.current().nextInt(1,10);
+	int randResponse = ThreadLocalRandom.current().nextInt(1,10);
 	
 	
     @Override
     public void onStart() {
-        // This gets executed when the script first starts.
-    	
-    		
     }
     
     private enum State {
-		BANK_INVENTORY, WAIT, SHOP, HOP, LOGOUT// Declares the different states of the program.
+		BANK_INVENTORY, SHOP, HOP, CHAT1// Declares the different states of the program.
 	};
 
 	private State getState()
 	{	
+		RS2Widget response = getWidgets().get(162, 41);
 		if(getStore().isOpen()) {
-			if(getStore().getAmount("Battlestaff") <= 0 && getStore().getAmount("Nature rune") <= 0 && getStore().getAmount("Death rune") >= 0) {
+			if(getStore().getAmount(items) <= 0) {
 				return State.HOP;
 			}
 		}
-		if(getInventory().getAmount("Coins") <= 10000) {
-			return State.LOGOUT;
+		
+		
+		if(inventory.isFull()) {
+			return State.BANK_INVENTORY;
 		}
-		if(getStore().isOpen()) {
-			if(getStore().getAmount("Battlestaff") <= 0 && getStore().getAmount("Nature rune") <= 0 && getStore().getAmount("Death rune") >= 0 && inventory.isFull()) {
-				return State.BANK_INVENTORY;
-			}
+		
+		if(response.getMessage().contains("hi") || response.getMessage().contains("hey") || response.getMessage().contains("sup") ||
+		   response.getMessage().contains("yo") || response.getMessage().contains("hello") || response.getMessage().contains("heya")) {
+			return State.CHAT1;
 		}
 		
 		return State.SHOP;
@@ -59,22 +64,50 @@ public class Main extends Script {
     	State state = getState();
     	log(state);
     	switch  (state) {
-    	case SHOP:
+    	case SHOP: 
+    		RS2Widget chatBox  = getWidgets().get(162, 43, 0); 
+    		rand = ThreadLocalRandom.current().nextInt(1,10);
+    		if(rand == 5) {
+    			getMouse().moveSlightly();
+    		}
+    		if(rand == 10) {
+    			getMouse().moveOutsideScreen();
+    			sleep(random(2000-7000));
+    		}
+    		NPC Store = npcs.closest("Magic Store owner");
+    		if(chatBox.getMessage().contains("coins")) {
+    			stop();
+    		}
     		if(magicGuild.contains(myPlayer())) {    			
-    			if(getStore().isOpen()) {
+    			if(Store != null && getStore().isOpen()) {
     				if(getStore().getAmount("Death rune") >= 1) {
-    					getStore().buy("Death rune", 10);
+    					getStore().buy("Death rune", 10); 
+    					if(getStore().getAmount("Chaos rune") >= 1) {
+    						getStore().buy("Chaos rune", 10);
+    					}
     				}
     				if(getStore().getAmount("Nature rune") >= 1) {
     					getStore().buy("Nature rune", 10);
+    					if(getStore().getAmount("Chaos rune") >= 1) {
+    						getStore().buy("Chaos rune", 10);
+    					}
     				}
     				if(getStore().getAmount("Battlestaff") >= 1) {
     					getStore().buy("Battlestaff", 10);
-    				}
-    				
+    					if(getStore().getAmount("Chaos rune") >= 1) {
+    						getStore().buy("Chaos rune", 10);
+    					}
+    				}   				
     			}
-    			if(!getStore().isOpen()) {
+    			if(!getStore().isOpen() && Store != null) {
     				Store.interact("Trade");
+    				new ConditionalSleep(7000, 10000) {
+    					@Override
+    					
+    					public boolean condition() throws InterruptedException {
+    						return getStore().isOpen();
+    					}
+    				}.sleep();
     			}
     		}
     		else {    		
@@ -82,7 +115,34 @@ public class Main extends Script {
     		}
     		break;          
     	
-    	case HOP:      	
+    	case HOP:   
+    		rand = ThreadLocalRandom.current().nextInt(1,10);
+    		if(rand == 5) {
+    			getMouse().moveSlightly();
+    		}
+    		if(rand == 10) {
+    			getMouse().moveOutsideScreen();
+    			sleep(random(2000-7000));
+    		}
+    		if(rand == 2) {
+    			getMouse().moveOutsideScreen();
+    			sleep(random(7000-13000));
+    		}
+    		if(rand == 7) {
+    			tabs.open(Tab.IGNORES);
+    			sleep(random(200,500));    			
+    		}
+    		if(getStore().isOpen()) {
+				getStore().close();
+				new ConditionalSleep(20000, 25000) {
+					@Override
+					
+					public boolean condition() throws InterruptedException {
+						return !getStore().isOpen();
+					}
+				}.sleep();
+			}
+    		sleep(random(1000,4000));
     		getWorlds().hopToP2PWorld();
     		new ConditionalSleep(20000, 25000) {
 				@Override
@@ -92,12 +152,18 @@ public class Main extends Script {
 				}
 			}.sleep();
 			
-    		break;  
-		
-    	case WAIT:
-    		break;    		
+    		break;  	
+    	   		
     		
-    	case BANK_INVENTORY: // Opens a nearby bank and deposits everything in the inventory except the knife.	
+    	case BANK_INVENTORY: 
+    		rand = ThreadLocalRandom.current().nextInt(1,10);
+    		if(rand == 5) {
+    			getMouse().moveSlightly();
+    		}
+    		if(rand == 10) {
+    			getMouse().moveOutsideScreen();
+    			sleep(random(2000-7000));
+    		}
     		if(yanilleBank.contains(myPlayer()))
 			{
 				if(!getBank().isOpen())
@@ -123,9 +189,36 @@ public class Main extends Script {
 			}
     		break; 
     		
-    	case LOGOUT:
-    		stop();
-    		break;   
+    	case CHAT1:
+    		
+    		randResponse = ThreadLocalRandom.current().nextInt(1,10);
+    		if(randResponse == 1)
+    			getKeyboard().typeString("hi");
+    		if(randResponse == 2)
+    			getKeyboard().typeString("heya");
+    		if(randResponse == 3)
+    			getKeyboard().typeString("sup");
+    		if(randResponse == 4)
+    			getKeyboard().typeString("yo");
+    		if(randResponse == 5)
+    			getKeyboard().typeString("hihi");
+    		if(randResponse == 6)
+    			getKeyboard().typeString("hallo");
+    		if(randResponse == 7)
+    			getKeyboard().typeString("hello");
+    		if(randResponse == 8)
+    			getKeyboard().typeString("hiiiii");
+    		if(randResponse == 9)
+    			getKeyboard().typeString("howdy");
+    		if(randResponse == 10)
+    			getKeyboard().typeString("heeey");
+    		else
+    			getKeyboard().typeString("que?");
+    		
+    		
+    		break;
+    		
+    	 
     	}
     	 
     	
